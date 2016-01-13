f8ad2e4f93afd53e5346666f7fa9690dfbd0b9b8
Freddy Calk <freddycalk@gmail.com>
2016-01-13 11:15:07 -0500

    yikyak app clone
    init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="assets/compass/css/style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	
+</body>
+</html> -->
+
+<?php print "<h1> HOME PAGE</h1>"; ?>
