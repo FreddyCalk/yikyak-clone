4cd876db7ff96a413ca7734e362fb2d2d2b51dfa
Freddy Calk <freddycalk@gmail.com>
2016-01-14 11:41:41 -0500

    header knows who the user is after they login

--- header.php
+++ header.php
@@ -1,11 +1,18 @@
-	<div id='button-wrapper' style="float:right">
-		<?php
-			if($_SESSION['username']){
-				print '<a href="/index.php?logout=true" class="btn btn-danger">Logout</a>';
-			}else{
-				print '<a href="/login.php" class="btn btn-success">Login</a>';
-				print '<a href="/signup.php" class="btn btn-primary">Register</a>';
-			}
-		?>
+<div id="header-wrapper">	
+	<div class="container">
+		<div class="row">
+			<div id='button-wrapper' style="float:right">
+				<?php
+					if(isset($_SESSION['username'])){
+						print 'Welcome Back, <a href="#">' . $_SESSION['username'] . '</a>&nbsp &nbsp &nbsp';
+						print '<a href="/index.php?logout=true" class="btn btn-danger">Logout</a>';
+					}else{
+						print '<a href="/login.php" class="btn btn-primary">Login</a>';
+						print '<a href="/signup.php" class="btn btn-success">Register</a>';
+					}
+				?>
+			</div>
+		<?php print "<a href='/' class='btn btn-primary'>Yik Yak Home</a>"; ?>
+		</div>
 	</div>
-	<?php print "<a id='home-link' href='index.php' style='display:inline'><h1 style='display:inline'> &nbsp Freddy's YIK YAK</h1></a>"; ?>
+</div>
--- index.php
+++ index.php
@@ -24,9 +24,7 @@
 </head>
 <body>
 
-	<?php
-		include 'header.php';
-	?>
+	<?php include 'header.php'; ?>
 
 	
 	
