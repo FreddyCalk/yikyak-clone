fdedf117427d3050e8dcd8f9c3d4bbd96471f269
Freddy Calk <freddycalk@gmail.com>
2016-01-28 19:36:40 -0500

    updated vote processing
    fixed bug that would change the votes on every post if the user decided
    to change his/her mind on a single post

--- process_vote.php
+++ process_vote.php
@@ -8,15 +8,17 @@
 	}
 
 	$result = DB::query("SELECT * FROM post_votes WHERE uid='".$_GET['uid']."' AND pid ='".$_GET['pid']."'");
-
+	$pid = $_GET['pid'];
+	$uid = $_GET['uid'];
 	// print '<pre>';
+	// print_r($_GET);
 	// print_r($result);
 	// exit;
 
 	if((sizeof($result)>0)&&($result[0]['vote'] == 1)&&($vote == -1)){
-		DB::update('post_votes', array('vote' => $vote),"pid=%i" AND "uid=%i",$_GET['pid'],$_GET['uid']);
+		DB::query("UPDATE post_votes SET vote = $vote WHERE pid = $pid AND uid = $uid");
 	}else if((sizeof($result)>0)&&($result[0]['vote'] == -1)&&($vote == 1)){
-		DB::update('post_votes', array('vote' => $vote),"pid=%i" AND "uid=%i",$_GET['pid'],$_GET['uid']);
+		DB::query("UPDATE post_votes SET vote = $vote WHERE pid = $pid AND uid = $uid");
 	}else if(sizeof($result)==0){
 		DB::insert('post_votes', array(
 		'pid' => $_GET['pid'],
